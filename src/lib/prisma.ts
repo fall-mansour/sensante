1532d183ddd5d2c88f571588022b60d330d3422a
idrissa seck sow <sowidrissaseck@gmail.com>
2026-05-01 09:52:46 +0000

    correction des fichiers prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+/* import { PrismaClient } from "@prisma/client";
 const globalForPrisma = globalThis as unknown as {
 prisma: PrismaClient | undefined;
 };
@@ -6,3 +6,14 @@ export const prisma =
 globalForPrisma.prisma ?? new PrismaClient();
 if (process.env.NODE_ENV !== "production")
 globalForPrisma.prisma = prisma;
+*/
+
+import { PrismaClient } from '@prisma/client'
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
