902d74ef20ac7418be0a74ed3cb6fe804b6f4410
fall-mansour <mansourbabydriver221@gmail.com>
2026-05-01 22:13:33 +0000

    correction layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,32 +20,14 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body className={inter.className}>
-        <div className="flex flex-col min-h-screen">
-          <Header />
-          <div className="flex flex-1">
-            <Sidebar />
-            <main className="flex-1 bg-gray-50 p-8">{children}</main>
-          </div>
-        </div>
-      </body>
-    </html>
-  );
-}
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="fr">
-      <body className={inter.className}>
         <SessionWrapper>
           <div className="flex flex-col min-h-screen">
             <Header />
-            <div className="flex flex -1">
+            <div className="flex flex-1">
               <Sidebar />
-              <main className="flex -1 bg-gray -50 p-8">{children}</main>
+              <main className="flex-1 bg-gray-50 p-8">
+                {children}
+              </main>
             </div>
           </div>
         </SessionWrapper>
