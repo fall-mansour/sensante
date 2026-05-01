874d77bde05642f882310fde5a35cf78f3ce2e8a
idrissa seck sow <sowidrissaseck@gmail.com>
2026-05-01 20:42:45 +0000

    correction du doc register

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,24 +32,3 @@ export default function RootLayout({
   );
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="fr">
-      <body className={inter.className}>
-        <SessionWrapper>
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <div className="flex flex -1">
-              <Sidebar />
-              <main className="flex -1 bg-gray -50 p-8">{children}</main>
-            </div>
-          </div>
-        </SessionWrapper>
-      </body>
-    </html>
-  );
-}
